b3c8464f2285e5f5730017437f91426f24b7c88b
Mehdi <mehdi.medimegh@isamm.u-manouba.tn>
2022-10-26 21:06:58 +0100

    latest version

---
+++ Assets/Scripts/ThirdPerson.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPerson : MonoBehaviour
+{
+
+    public FixedJoyStick LeftJoystick;
+    public FixedButton Button;
+    public FixedTouchfield Touchfield;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
